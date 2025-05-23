39ace262648d76260f0fbd8ab0834d49fabf27ed
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-23 17:37:11 +0200

    feat:/ create user functionality updated successfully

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,11 @@
+import User from "../models/userModel.js"
+
+export const createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
