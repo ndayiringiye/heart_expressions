c54940d44e2c1fa41bfbe3c273fffd1299f41c22
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-23 18:02:28 +0200

    feat/subscribe functionality provided successfully

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, getSingleUser } from "../controllers/userController.js";
+import { createSubscribing } from "../controllers/subscribeController.js";
 
 const router = express.Router();
 
 router.post("/api/user", createUser);
+router.get("/api/user/:id", getSingleUser);
+router.post("/api/users/:userId/subscribe", createSubscribing)
 
 export default router;
