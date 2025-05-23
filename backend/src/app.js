054d8da03ce2f7c733296eb4afba54811f190df3
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-23 17:14:31 +0200

    feat:/user schema added successfully

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
-
+import cors from "cors"
+import dotenv from "dotenv";
 const app = express()
+dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 app.get("/", (req , res) =>{
     res.send("gettted one")
-})
+});
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () =>{
-    console.log(`server is run on port 5000`)
+app.listen(PORT, () =>{
+    console.log(`server is run on port ${PORT}`)
 })
